79dd70172d4710ce183b1625ff4973895f81aa41
Warrd Adlani <warrdadlani@gmail.com>
2022-09-02 17:57:14 +0100

    + cosmetic

---
+++ Presentation/Presentation/Shared/UITextField+Extensions.swift
@@ -0,0 +1,38 @@
+//
+//  UITextField+Extensions.swift
+//  Presentation
+//
+//  Created by Warrd Adlani on 02/09/2022.
+//
+
+import Foundation
+import UIKit
+
+public class TextField: UITextField {
+
+    public let padding = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+    public func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    public func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
--- YourLine/YourLine/Views/SignView/SignInViewController.swift
+++ YourLine/YourLine/Views/SignView/SignInViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import Presentation
 
 class SignInViewController: UIViewController, SignInViewing {
+    
+    @IBOutlet weak var passwordField: TextField!
+    @IBOutlet weak var emailField: TextField!
     
     private var presenter: SignInPresenting?
 
@@ -15,6 +19,7 @@ class SignInViewController: UIViewController, SignInViewing {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
     
     func setPresenter(_ presenter: SignInPresenter?) {
@@ -26,9 +31,29 @@ class SignInViewController: UIViewController, SignInViewing {
     }
     
     @IBAction func didTapSignInButton(_ sender: Any) {
-        presenter?.signIn()
+        
+        let password = passwordField.text ?? ""
+        let email = emailField.text ?? ""
+        
+        if email.contains("your@line.com"), password.contains("12345678") {
+            presenter?.signIn()
+        } else {
+            showAlert(with: "Error", message: "Please enter an email and password")
+        }
     }
     
     @IBAction func unwind( _ seg: UIStoryboardSegue) {
+    }
+    
+    func showAlert(with title: String, message: String) {
+        
+        let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        
+        let okayAction = UIAlertAction(title: "Okay", style: .cancel)
+        
+        controller.addAction(okayAction)
+        
+        present(controller, animated: true)
     }
 }
